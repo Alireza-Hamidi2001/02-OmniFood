ba774a09cccee5f8e236aa801184bb4f579d207b
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2025-09-01 11:13:09 +0330

    second commit

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 const sections = document.querySelectorAll("section");
-let current = 0;
+let currentSection = 0;
 let isScrolling = false;
 
 function scrollToSection(index) {
     if (index < 0 || index >= sections.length) return;
     isScrolling = true;
-    const target = sections[index].offsetTop;
-    lenis.scrollTo(target, {
-        onComplete: () => {
-            isScrolling = false;
-        },
-    });
+    sections[index].scrollIntoView({ behavior: "smooth" });
+    currentSection = index;
+
+    setTimeout(() => {
+        isScrolling = false;
+    }, 1000);
 }
 
-// کیبورد
-document.addEventListener("keydown", (e) => {
-    console.log(e.key);
+window.addEventListener("wheel", (e) => {
     if (isScrolling) return;
-    if (e.key === "ArrowDown") {
-        current = Math.min(current + 1, sections.length - 1);
-        scrollToSection(current);
-    } else if (e.key === "ArrowUp") {
-        current = Math.max(current - 1, 0);
-        scrollToSection(current);
+
+    if (e.deltaY > 0) {
+        scrollToSection(currentSection + 1);
+    } else if (e.deltaY < 0) {
+        scrollToSection(currentSection - 1);
     }
 });
 
-// موس
-// غلتک موس با debounce کوچک
-document.addEventListener("wheel", (e) => {
+window.addEventListener("keydown", (e) => {
     if (isScrolling) return;
-    clearTimeout(wheelTimeout);
-    wheelTimeout = setTimeout(() => {
-        if (e.deltaY > 0) {
-            current = Math.min(current + 1, sections.length - 1);
-        } else if (e.deltaY < 0) {
-            current = Math.max(current - 1, 0);
-        }
-        scrollToSection(current);
-    }, 50);
+
+    if (e.key === "ArrowDown") {
+        scrollToSection(currentSection + 1);
+    } else if (e.key === "ArrowUp") {
+        scrollToSection(currentSection - 1);
+    }
 });
